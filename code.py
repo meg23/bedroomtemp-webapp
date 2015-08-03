17ff63571dcb9b3af71d0e98ff1d2d445e9bcdf9
meg23 <m.gelman08@gmail.com>
2015-08-03 17:50:59 -0400

    Mixxed changes.

--- code.py
+++ code.py
@@ -9,31 +9,29 @@ from datetime import datetime, date, time
 from google.appengine.ext.webapp import template
 from google.appengine.ext import db
 from season import get_season
+from photon import Photon
+from google.appengine.api import memcache
+from google.appengine.ext import deferred
 
 log = logging.getLogger('webapp')
 
-class Temperature(db.Model):
-    temp = db.IntegerProperty(required=True);
-    date = db.DateTimeProperty(auto_now_add=True)
-
-class User(db.Model):
-    email = db.StringProperty();
-    authtoken = db.StringProperty();
-
-class Rest(webapp2.RequestHandler):
-    def post(self):
-        temp = int(self.request.get('temp'))
-        reading = Temperature(temp=temp)
-        reading.put()
-
 class Main(webapp2.RequestHandler):
+
     def get(self):
-        tempQuery = db.GqlQuery("SELECT * FROM Temperature ORDER BY date DESC")
-        last_entry = tempQuery.get()
-        current_temp = last_entry.temp
+
+        # defer fetch task to backend
+        photon = Photon()
+        result = deferred.defer(photon.get_bedroomtemp)
+
+        # get value from cache
+        current_temp =  memcache.get('current_temp') or 0
+
         season = get_season(datetime.now())
         message = ""
-        if current_temp < 65:
+        
+        if current_temp == 0:
+            message = "Yep, somethings broken over here"
+        elif current_temp < 65 and current_temp != 0:
             message = "Pretty cool right now"
         elif season == "summer" and current_temp < 73 and current_temp > 65:
             message = "Pretty comfortable right now"
@@ -43,12 +41,15 @@ class Main(webapp2.RequestHandler):
             message = "Hotter Than a parking lot on Mars." 
         else:
             message = "Not sure what to make of the weather"
+
         template_values = {
             'current_temp': current_temp,
             'message': message
         }
+
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
 
-app = webapp2.WSGIApplication([('/api.*', Rest), ('/', Main)], debug=True)
+
+app = webapp2.WSGIApplication([('/', Main)], debug=True)
 
--- temperature.py
+++
@@ -1,5 +0,0 @@
-from google.appengine.ext import db
-
-class Temperature(db.Model):
-    temp = db.IntegerProperty(required=True);
-    date = db.DateTimeProperty(auto_now_add=True)
